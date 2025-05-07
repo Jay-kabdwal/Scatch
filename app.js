6ad5b6c9f2dd5c7d3742a03fc5d0ea05cdb83847
jay-kabdwal <jaykabdwal@gmail.com>
2025-05-07 15:52:16 +0530

    auth

--- app.js
+++ app.js
@@ -5,8 +5,6 @@ const db = require("./config/mongoose-connection");
 const adminsRouter = require("./routes/adminsRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
-const register = require("./routes/register");
-const login = require("./routes/login");
 
 require("dotenv").config()
 
@@ -18,11 +16,11 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/", register);
-app.use("/user", login);
+app.use("/", usersRouter);
+app.use("/user", usersRouter );
 app.use("/admin", adminsRouter);
 app.use("/product", productsRouter);
-app.use("/user",  register);
+// app.use("/user",  register);
 
 
 app.listen(3000, () => {
