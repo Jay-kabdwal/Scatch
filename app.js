c443fb4e82ff10a4e143e312a7bace48ca12d537
jay-kabdwal <jaykabdwal@gmail.com>
2025-05-08 23:39:50 +0530

    corrected

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const db = require("./config/mongoose-connection");
 const adminsRouter = require("./routes/adminsRouter");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
+const indexRouter = require("./routes/indexRouter");
+const expressSession = require("express-session");
+const flash = require("connect-flash");
 
 require("dotenv").config()
 
@@ -15,12 +18,26 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
+app.use(expressSession({
+    secret: process.env.EXPRESS_SESSION_SECRET || 'your-secret-key',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using HTTPS
+}));
+app.use(flash());
 
-app.use("/", usersRouter);
+app.use("/", indexRouter);
 app.use("/user", usersRouter );
 app.use("/admin", adminsRouter);
 app.use("/product", productsRouter);
 
+// Error handler for 404
+app.use((req, res, next) => {
+    res.status(404).render('error', { 
+        error: 'Page not found',
+        message: '' 
+    });
+});
 
 app.listen(3000, () => {
     console.log('Server running on http://localhost:3000');
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -2,16 +2,6 @@ const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 
-// Render register page
-router.get("/", (req, res) => {
-  res.render("register", { error: "", message: "" });
-});
-
-// Render login page
-router.get("/login", (req, res) => {
-  res.render("login", { error: "", message: "" });
-});
-
 // Handle post requests
 router.post("/register", registerUser);
 router.post("/login", loginUser);
